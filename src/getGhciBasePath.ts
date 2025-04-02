13ad9b6a60d88c7428eaa1d3c8edc326bd4e5863
Mike Hodnick <mike@kindohm.com>
2025-04-02 12:47:16 -0500

    attempt to write a test

--- src/getGhciBasePath.ts
+++ src/getGhciBasePath.ts
@@ -1,6 +1,18 @@
 import * as os from 'os';
 import * as path from 'path';
+import { ghciPath } from './config';
+import { writeLine } from './logger';
 
 export const getGhciBasePath = () => {
-  return path.join(os.homedir(), ".ghcup", "bin")
+    const configuredPath = ghciPath();
+
+    if (configuredPath && configuredPath.trim().length > 0) {
+        writeLine(`custom GHCI base path configured at ${configuredPath}`);
+        const resolvedPath = configuredPath.replace('~', os.homedir());
+        return resolvedPath.endsWith('ghci')
+            ? resolvedPath.substring(0, resolvedPath.length - 4)
+            : resolvedPath;
+    }
+
+    return path.join(os.homedir(), '.ghcup', 'bin');
 };
