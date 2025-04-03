2688d9150cd13c49f45f9e474b831b4f57070241
Mike Hodnick <mike@kindohm.com>
2025-04-03 12:39:56 -0500

    remove console logs

--- src/getTidalBootPath.ts
+++ src/getTidalBootPath.ts
@@ -6,47 +6,50 @@ import * as path from 'path';
 import { getGhciBasePath } from './getGhciBasePath';
 import * as child_process from 'child_process';
 
+const bootTidalFileName = 'BootTidal.hs';
+
 export const getTidalBootPath = () => {
-    console.log('start')
-    const configuredBootTidalPath = config.bootTidalPath();
+  const configuredBootTidalPath = config.bootTidalPath();
 
-    // first, check for a configured boot path
-    if (configuredBootTidalPath) {
-        writeLine(
-            `Custom Tidal boot path is configured: ${configuredBootTidalPath}`
-        );
+  // first, check for a configured boot path
+  if (configuredBootTidalPath) {
+    writeLine(
+      `Custom Tidal boot path is configured: ${configuredBootTidalPath}`
+    );
 
-        return configuredBootTidalPath;
+    return configuredBootTidalPath;
+  }
+
+  writeLine('Custom Tidal boot path is not configured');
+
+  // next, check for a BootTidal.hs file in the local dir
+  if (workspace.workspaceFolders && workspace.workspaceFolders.length > 0) {
+    const localBootFilePath = path.join(
+      workspace.workspaceFolders[0].uri.fsPath,
+      bootTidalFileName
+    );
+    try {
+      fs.statSync(localBootFilePath);
+      writeLine(`Local Tidal boot file was found: ${localBootFilePath}`);
+      return localBootFilePath;
+    } catch (err) {
+      writeLine(`Local Tidal boot file was not found.`);
     }
+  }
 
-    writeLine('Custom Tidal boot path is not configured');
+  // finally, locate the Tidal boot file in the Tidal ghc package
+  const ghciBasePath = getGhciBasePath();
+  const command = `${path.join(ghciBasePath, 'ghc-pkg')} field tidal data-dir`;
+  const dataDir = child_process
+    .execSync(command)
+    .toString('utf8')
+    .replace('data-dir: ', '')
+    .trim();
 
-    // next, check for a BootTidal.hs file in the local dir
-    if (workspace.workspaceFolders && workspace.workspaceFolders.length > 0) {
-        const localBootFilePath = path.join(
-            workspace.workspaceFolders[0].uri.fsPath,
-            'BootTidal.hs'
-        );
-        try {
-            console.log('here')
-            fs.statSync(localBootFilePath);
-            writeLine(`Local Tidal boot file was found: ${localBootFilePath}`);
-            return localBootFilePath;
-        } catch (err) {
-            writeLine(`Local Tidal boot file was not found.`);
-        }
-    }
+  const packagePath = path.join(dataDir, bootTidalFileName);
 
-    // finally, locate the Tidal boot file in the Tidal ghc package
-    const ghciBasePath = getGhciBasePath();
-    const command = `${path.join(
-        ghciBasePath,
-        'ghc-pkg'
-    )} field tidal data-dir`;
-    const dataDir = child_process.execSync(command).toString('utf8').replace('data-dir: ', '').trim();
-
-    const packagePath = path.join(dataDir, 'BootTidal.hs');
-
-    writeLine(`Using default Tidal boot file from package path: "${packagePath}"`);
-    return packagePath;
+  writeLine(
+    `Using default Tidal boot file from package path: "${packagePath}"`
+  );
+  return packagePath;
 };
