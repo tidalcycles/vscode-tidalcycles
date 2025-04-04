f5a5d7c4d871a3c357869bb2d7d59ba797f3aa30
Mike Hodnick <mike@kindohm.com>
2025-04-03 19:52:27 -0500

    add rest of mutes

--- src/toggleMutesCommands.ts
+++ src/toggleMutesCommands.ts
@@ -7,22 +7,24 @@ const toggleMute = (connection: string) => {
     ? `unmute ${connection}`
     : `mute ${connection}`;
 
+  mutesState[connection] = !mutesState[connection];
+
   send(command);
 };
 
-export const toggleMute1 = () => toggleMute('d1');
-export const toggleMute2 = () => toggleMute('d2');
-export const toggleMute3 = () => toggleMute('d3');
-export const toggleMute4 = () => toggleMute('d4');
-export const toggleMute5 = () => toggleMute('d5');
-export const toggleMute6 = () => toggleMute('d6');
-export const toggleMute7 = () => toggleMute('d7');
-export const toggleMute8 = () => toggleMute('d8');
-export const toggleMute9 = () => toggleMute('d9');
-export const toggleMute10 = () => toggleMute('d10');
-export const toggleMute11 = () => toggleMute('d11');
-export const toggleMute12 = () => toggleMute('d12');
-export const toggleMute13 = () => toggleMute('d13');
-export const toggleMute14 = () => toggleMute('d14');
-export const toggleMute15 = () => toggleMute('d15');
-export const toggleMute16 = () => toggleMute('d16');
+export const toggleMute1 = () => toggleMute('1');
+export const toggleMute2 = () => toggleMute('2');
+export const toggleMute3 = () => toggleMute('3');
+export const toggleMute4 = () => toggleMute('4');
+export const toggleMute5 = () => toggleMute('5');
+export const toggleMute6 = () => toggleMute('6');
+export const toggleMute7 = () => toggleMute('7');
+export const toggleMute8 = () => toggleMute('8');
+export const toggleMute9 = () => toggleMute('9');
+export const toggleMute10 = () => toggleMute('10');
+export const toggleMute11 = () => toggleMute('11');
+export const toggleMute12 = () => toggleMute('12');
+export const toggleMute13 = () => toggleMute('13');
+export const toggleMute14 = () => toggleMute('14');
+export const toggleMute15 = () => toggleMute('15');
+export const toggleMute16 = () => toggleMute('16');
